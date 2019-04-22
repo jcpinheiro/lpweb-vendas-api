f927ae72e337d86eab088f165b54a80bfcf1d5bd
Joao Carlos <jcpinheiro@ifma.edu.br>
2019-04-22 17:59:03 -0300

    Commit inicial do projeto com Spring Boot

---
+++ src/main/java/dcomp/lpweb/vendas/api/VendasApiApplication.java
@@ -0,0 +1,13 @@
+package dcomp.lpweb.vendas.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApiApplication.class, args);
+    }
+
+}
